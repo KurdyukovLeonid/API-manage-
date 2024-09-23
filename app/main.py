c432ac6b9d9cf333123108207b36ea2c23ef8839
kurdyukov <leonid.kurdyukov@icloud.com>
2024-09-23 23:00:10 +0300

    Переделанное дз

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from routers import task, user
+from fastapi.routing import APIRouter
+from routers.task import router as task_router
+from routers.user import router as user_router
 
 app = FastAPI()
 
-@app.get('/')
-async def welcome():
-    return {'message': 'Welcome to Taskmanager'}
+@app.get("/")
+async def read_root():
+    return {"message": "Welcome to Taskmanager"}
 
-
-
-app.include_router(user.router)
-app.include_router(task.router)
+app.include_router(task_router)
+app.include_router(user_router)
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,71 +1,23 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.orm import Session
-from backend.db_depends import get_db
-from typing import Annotated, List
-from models import User, Task
-from schemas import CreateUser, UpdateUser
-from sqlalchemy import select, insert, update, delete
+from fastapi import APIRouter
 
-router = APIRouter(prefix='/user', tags=['user'])
+router = APIRouter(prefix="/user", tags=["user"])
 
+@router.get("/")
+async def all_users():
+    pass
 
-@router.get('/', response_model=List[User])
-async def all_users(db: Annotated[Session, Depends(get_db)]):
-    result = db.execute(select(User)).scalars().all()
-    return result
+@router.get("/{user_id}")
+async def user_by_id(user_id: int):
+    pass
 
+@router.post("/create")
+async def create_user():
+    pass
 
-@router.get('/{user_id}', response_model=User)
-async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    user = db.execute(select(User).where(User.id == user_id)).scalar_one_or_none()
-    if user is None:
-        raise HTTPException(status_code=404, detail="User was not found")
-    return user
+@router.put("/update")
+async def update_user():
+    pass
 
-
-@router.post('/create', status_code=status.HTTP_201_CREATED)
-async def create_user(user: CreateUser, db: Annotated[Session, Depends(get_db)]):
-    new_user = User(
-        username=user.username,
-        firstname=user.firstname,
-        secondname=user.secondname,
-        age=user.age
-    )
-    db.execute(insert(User).values(new_user))
-    db.commit()
-    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
-
-
-@router.put('/update/{user_id}', status_code=status.HTTP_200_OK)
-async def update_user(user_id: int, user: UpdateUser, db: Annotated[Session, Depends(get_db)]):
-    stmt = update(User).where(User.id == user_id).values(
-        firstname=user.firstname,
-        secondname=user.secondname,
-        age=user.age
-    )
-    result = db.execute(stmt)
-    db.commit()
-
-    if result.rowcount == 0:
-        raise HTTPException(status_code=404, detail="User was not found")
-
-    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
-
-
-@router.delete('/delete/{user_id}', status_code=status.HTTP_200_OK)
-async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    db.execute(delete(Task).where(Task.user_id == user_id))
-
-    stmt = delete(User).where(User.id == user_id)
-    result = db.execute(stmt)
-    db.commit()
-
-    if result.rowcount == 0:
-        raise HTTPException(status_code=404, detail="User was not found")
-
-    return {'status_code': status.HTTP_200_OK, 'transaction': 'User and related tasks deleted successfully!'}
-
-@router.get('/{user_id}/tasks', response_model=List[Task])
-async def tasks_by_user_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    tasks = db.execute(select(Task).where(Task.user_id == user_id)).scalars().all()
-    return tasks
+@router.delete("/delete")
+async def delete_user():
+    pass
